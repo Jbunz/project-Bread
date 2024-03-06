8cc14a779f2ed0f67f94f25f5edc1191f05e48ed
Jbunz <jacob652@aol.com>
2024-03-05 21:11:31 -0800

    added a bunch O stuff

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,17 +1,35 @@
-const router = require('express').Router()
-const Bread = require('../models/bread')
+const router = require("express").Router();
+const Bread = require("../models/bread");
 
 // GET retrieve all the bread
-router.get('/', (req, res) => {
-    res.render('index', {
-        breads: Bread
-    })
-})
+router.get("/", (req, res) => {
+  res.render("index", {
+    breads: Bread,
+  });
+});
+
+router.get("/new", (req, res) => {
+  res.render("new");
+});
 
 // GET retreive bread by index
-router.get('/:index', (req, res) => {
-    const { index } = req.params
-    res.send(Bread[index])
-})
+router.get("/:index", (req, res) => {
+  const { index } = req.params;
+  res.render("show", {
+    bread: Bread[index],
+  });
+});
 
-module.exports = router
+//CREATE
+router.post("/", (req, res) => {
+    if (!req.body.image) req.body.image = 'https://houseofnasheats.com/wp-content/uploads/2022/02/French-Bread-1.jpg'
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
+  } else {
+    req.body.hasGluten = false;
+  }
+  Bread.push(req.body);
+  res.redirect("/bread");
+});
+
+module.exports = router;
